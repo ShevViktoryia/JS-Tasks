1e16001a653fe95e98b21200570be3b60400e4c6
Viktoryia Shevchenko <shevviktoryia@gmail.com>
2023-02-08 22:39:01 +0300

    feat: add task 2 unit 5

--- 5.js
+++ 5.js
@@ -2,3 +2,12 @@
 //Вам понадобятся методы строк.
 
 const ucFirst = str => str.charAt(0).toUpperCase() + str.slice(1);
+
+//Напишите функцию checkSpam(str), возвращающую true, если str содержит 'badWord' или 'XXX', а иначе false.
+//Функция должна быть нечувствительна к регистру.
+
+const checkSpam = str => {
+  const substr1 = 'badWord', substr2 = 'XXX';
+
+  return (str.toLowerCase().includes(substr1.toLowerCase()) || str.toLowerCase().includes(substr2.toLowerCase()));
+}
