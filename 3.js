eeb99b3949bb158551fe5a18a0d3853490599ad1
Viktoryia Shevchenko <shevviktoryia@gmail.com>
2023-02-09 19:55:54 +0300

    feat: add task 5 unit 3

--- 3.js
+++ 3.js
@@ -46,3 +46,34 @@ const consoleArr = numbers => {
     console.log(`Индексу ${i} соответствует число ${numbers[i]}`);
   }
 }
+
+/*Дан массив объектов, например:
+let questions = [{
+    question: "What's the currency of the USA?",
+    choices: ["US dollar", "Ruble", "Horses", "Gold"],
+    corAnswer: 0
+}, {
+    question: "Where was the American Declaration of Independence signed?",
+    choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+    corAnswer: 0
+}];
+
+Добавить в каждый объект дополнительное поле usersAnswer со значением null.
+Решение должно работать для массива любой длины.
+*/
+
+let questions = [{
+  question: "What's the currency of the USA?",
+  choices: ["US dollar", "Ruble", "Horses", "Gold"],
+  corAnswer: 0
+}, {
+  question: "Where was the American Declaration of Independence signed?",
+  choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+  corAnswer: 0
+}];
+const addProperty = arr => {
+  arr.forEach(item => {
+    item.usersAnswer = "null";
+  });
+  return arr;
+}
