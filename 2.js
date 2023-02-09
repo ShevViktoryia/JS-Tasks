e55c0a9d3582d167f1963a1aff34f3f4d61b16bb
Viktoryia Shevchenko <shevviktoryia@gmail.com>
2023-02-09 20:52:10 +0300

    feat: fix task 1 unit 2

---
+++ 2.js
@@ -0,0 +1,15 @@
+/*
+Перепишите код с использованием тернарного оператора
+
+let country = 'Sweden';
+let access;
+if (country == 'Sweden') {
+access = true;
+} else{
+access = false;
+}
+*/
+let country = 'Sweden', access;
+access = country == 'Sweden' ? true : false;
+
+
