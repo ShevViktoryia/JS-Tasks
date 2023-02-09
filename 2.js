1bebe89c8b7de577a9b947301476ae310dff42f0
Viktoryia Shevchenko <shevviktoryia@gmail.com>
2023-02-09 20:54:29 +0300

    feat: add task 2 unit 2

--- 2.js
+++ 2.js
@@ -11,3 +11,13 @@ access = false;
 */
 let country = 'Sweden', access;
 access = country == 'Sweden' ? true : false;
+
+/*
+Создать переменную и записать в нее число, например 10.
+10 раз увеличить значение этой переменной на 1. Результат (1 шт.) вывести в консоль.
+*/
+let a = 10;
+for(let i = 0; i < 10; i++) {
+  a++;
+}
+console.log(a);
