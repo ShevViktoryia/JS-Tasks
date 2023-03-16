7a92f38d8c4706ec29220dab007ebbdde1e90412
Viktoryia Shevchenko <shevviktoryia@gmail.com>
2023-03-16 21:42:22 +0300

    feat: update task 2 unit 11

--- 11_unit/task 2/2.js
+++ 11_unit/task 2/2.js
@@ -2,10 +2,19 @@
 Передать массив в функцию. Внутри функции каждый параметр должен быть равен одному из элементов массива. */
 
 const names = ['John', 'Sam', 'Peter', 'Kate', 'Alex'];
-function consoleElem(first, second, third) {
+function consoleElem(arr) {
+  const [first, second, third] = arr;
   console.log(first);
   console.log(second);
   console.log(third);
 }
 
-consoleElem(...names);
+consoleElem(names);
+
+function consoleElem([first, second, third]) {
+  console.log(first);
+  console.log(second);
+  console.log(third);
+}
+
+consoleElem(names);
