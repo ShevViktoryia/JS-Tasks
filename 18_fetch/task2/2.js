dc3f8386ad5d27398c4426ea78eecf8d87989e17
Viktoryia Shevchenko <shevviktoryia@gmail.com>
2023-04-04 19:53:54 +0300

    feat: add task 1 unit 19

---
+++ 18_fetch/task2/2.js
@@ -0,0 +1,11 @@
+// fetch('https://api.emojisworld.fr/v1/random', {
+//   headers: {
+//     Accept: "application/json"
+//   }
+// })
+// .then(response => response.json())
+// .then(data => 
+//   {
+//     div.textContent = '';
+//     div.textContent = data.results[0].emoji;
+//   });
