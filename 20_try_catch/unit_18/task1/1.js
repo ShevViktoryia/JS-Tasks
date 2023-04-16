bd4a032c64a6536e9eae7715ff63be9c21be603c
Viktoryia Shevchenko <shevviktoryia@gmail.com>
2023-04-16 18:47:02 +0300

    feat: update unit 20

---
+++ 20_try_catch/unit_18/task1/1.js
@@ -0,0 +1,82 @@
+/*
+1.1. Добавить в html кнопку «Получить эмодзи дня», под ней пустой див для результата. 
+По нажатию кнопки отправлять запрос за рандомным эмодзи, отобразить его в диве под кнопкой,
+ размером примерно 70px (это можно сделать с помощью CSS-свойства font-size).
+
+1.2. Отправить запрос за всеми эмодзи, разместить их на странице по категориям
+*/
+
+const button = document.createElement('button');
+button.textContent = 'Получить эмодзи дня';
+document.body.append(button);
+
+const div = document.createElement('div');
+div.className = 'randomEmoji';
+document.body.append(div);
+
+button.onclick = () => {
+    fetch('https://api.emojisworld.fr/v1/random', {
+      headers: {
+        Accept: "application/json"
+      }
+    })
+    .then(response => response.json())
+    .then(data => 
+      {
+        div.textContent = '';
+        div.textContent = data.results[0].emoji;
+      })
+    .catch(error => {
+      console.log("Error ", error);
+    })
+}
+
+fetch('https://api.emojisworld.fr/v1/popular?&categories=10,9,7,8,6,5,4,3,2,1', {
+  headers: {
+    Accept: "application/json"
+  }
+})
+.then(response => response.json())
+.then(data => {
+  const block = document.createElement('div');
+  block.className = 'block';
+  document.body.append(block);
+  let arrNames = [];
+  for(let i = 0; i < data.results.length; i++) {
+    const category = document.createElement('div');
+    category.className = 'category';
+
+    const header = document.createElement('h1');
+    header.className = 'header';
+    const emojiGroup = document.createElement('div');
+    emojiGroup.id = 'add';
+
+    if(!arrNames.includes(data.results[i].category.name)) {
+      arrNames.push(data.results[i].category.name);
+      header.textContent = data.results[i].category.name;
+      emojiGroup.className = `${data.results[i].category.name}`;
+
+      category.append(header);
+      category.append(emojiGroup);
+      block.append(category);
+    }
+
+    const addClass = document.querySelectorAll('#add');
+    for(let j = 0; j < addClass.length; j++) {
+      data.results.forEach(item => {
+        switch(item.category.name) {
+          case `${addClass[j].className}`:
+            if(emojiGroup.className == addClass[j].className) {
+              emojiGroup.append(item.emoji);
+            }
+            break;
+          default:
+            break;
+        }
+      });
+    }
+  }
+})
+.catch(error => {
+  console.log("Error2 ", error);
+})
---
+++ 20_try_catch/unit_18/task3/3.js
@@ -0,0 +1,83 @@
+function createCard(card, data) {
+  const img = document.createElement('img');
+  img.className = 'card-img-top';
+  img.setAttribute('src' , `${data.flags.png}`);
+  const card_body = document.createElement('div');
+  card_body.className = 'card-body';
+  const card_title = document.createElement('h5');
+  card_title.className = 'card-title';
+  card_title.textContent = data.name.official;
+  const card_subtitle = document.createElement('h6');
+  card_subtitle.className = 'card-subtitle';
+  card_subtitle.textContent = data.continents;
+  const card_population = document.createElement('p');
+  card_population.className = 'card-text';
+  card_population.textContent = (data.population / 10**6).toFixed(2) + ' млн';
+  const card_languages = document.createElement('p');
+  card_languages.className = 'card-text';
+  for(let key in data.languages) {
+    card_languages.textContent += `${data.languages[key]} `;
+  }
+  const card_cur = document.createElement('p');
+  card_cur.className = 'card-text';
+  for(let key in data.currencies) {
+      card_cur.textContent += `${data.currencies[key]['symbol']} `+ `${data.currencies[key]['name']}`;
+  }
+  card.append(img);
+  card_body.append(card_title);
+  card_body.append(card_subtitle);
+  card_body.append(card_population);
+  card_body.append(card_languages);
+  card_body.append(card_cur);
+  card.append(card_body);
+}
+
+fetch('https://restcountries.com/v3.1/name/Spain', {
+  headers: {
+    Accept: "application/json"
+  }
+})
+.then(response => response.json())
+.then(data => {
+  const container = document.querySelector('.container');
+  
+  const row = document.createElement('div');
+  row.className = 'row';
+  const col = document.createElement('div');
+  col.className = 'col-4';
+  for(j = 0; j < 1; j++) {
+    const card = document.createElement('div');
+    card.className = 'card';
+    createCard(card, data[0]);
+    col.append(card);
+    row.append(col);
+    container.append(row);
+  }
+  const borders = data[0].borders;
+  borders.forEach(item => {
+    fetch(`https://restcountries.com/v3.1/alpha/${item}`)
+    .then(response => response.json())
+    .then(data => {
+      const container = document.querySelector('.container');
+        const col = document.createElement('div');
+        col.className = 'col-4';
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.style.width = '80%';
+        createCard(card, data[0]);
+        const card_header = document.createElement('h5');
+        card_header.className = 'card-header';
+        card_header.textContent = 'Соседняя страна';
+        card.prepend(card_header);
+        col.append(card);
+        row.append(col);
+        container.append(row);
+    })
+    .catch(error => {
+      console.log("Ошибка в запросе на соседние страны ", error);
+    })
+  })
+})
+.catch(error => {
+  console.log("Ошибка в основном запросе ", error);
+})
